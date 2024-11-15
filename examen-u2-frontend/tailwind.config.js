197fa046e2b60e4456cfc15344405925c1c691f2
annerh3 <annerh3@gmail.com>
2024-11-15 00:58:57 -0600

    Login Background changed --A

--- examen-u2-frontend/tailwind.config.js
+++ examen-u2-frontend/tailwind.config.js
@@ -7,7 +7,8 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-      "login-pattern": "url('https://i.postimg.cc/HxK4Pqcr/bg-login.jpg')",
+      // "login-pattern": "url('https://i.postimg.cc/HxK4Pqcr/bg-login.jpg')",
+       "login-pattern": "url('https://i.postimg.cc/tJp3JTZZ/DALL-E-2024-11-15-00-53-11-An-epic-battle-scene-between-the-web-browsers-Edge-and-Firefox-depicte.png')",
       
       },
     },
